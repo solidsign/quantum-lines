ff0b6009f8d21fdf00be14bcf50e3734e9aef2aa
solidius <49648821+solidsign@users.noreply.github.com>
2022-05-10 22:39:34 +0300

    добавил todo на тензорное произведение

---
+++ quantum-lines/Utils/MatrixTensorExtention.cs
@@ -0,0 +1,14 @@
+using System.Numerics;
+using MatrixDotNet;
+
+namespace quantum_lines.Utils
+{
+    public static class TensorMultiplier
+    {
+        public static Matrix<T> TensorMultiply<T>(params Matrix<T>[] matrices) where T : unmanaged
+        {
+            // TODO
+            return null;
+        }
+    }
+}
