c4abd912cbcb4f716d1bd130908158dfc165fb6d
solidius <49648821+solidsign@users.noreply.github.com>
2022-05-11 22:44:17 +0300

    реализовал тензорное произведение

--- quantum-lines/Utils/MatrixTensorExtention.cs
+++ quantum-lines/Utils/MatrixTensorExtention.cs
@@ -5,10 +5,36 @@ namespace quantum_lines.Utils
 {
     public static class TensorMultiplier
     {
-        public static Matrix<T> TensorMultiply<T>(params Matrix<T>[] matrices) where T : unmanaged
+        public static Matrix<Complex> Multiply(params Matrix<Complex>[] matrices)
         {
-            // TODO
-            return null;
+            var res = matrices[0];
+            for (int i = 1; i < matrices.Length; i++)
+            {
+                res = Multiply(res, matrices[i]);
+            }
+
+            return res;
+        }
+
+        private static Matrix<Complex> Multiply(Matrix<Complex> a, Matrix<Complex> b)
+        {
+            var matrix = new Matrix<Complex>(a.Rows * b.Rows, a.Columns * b.Columns);
+            
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Columns; j++)
+                {
+                    for (int k = 0; k < b.Rows; k++)
+                    {
+                        for (int l = 0; l < b.Columns; l++)
+                        {
+                            matrix[i * b.Rows + k, j * b.Columns + l] = a[i, j] * b[k, l];
+                        }
+                    }
+                }
+            }
+
+            return matrix;
         }
     }
 }
